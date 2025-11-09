bd82628fd8a2d79655fc800b15705f367c1b980a
Nathália Alves <Nathlia>
2025-11-09 12:33:55 -0300

    docs: execução da experiência 3 e adição no relatório

--- src/test3.sh
+++ src/test3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #SBATCH --output=test3.out
+#SBATCH --nodes=4
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=2
+#SBATCH --exclusive
 
 # Calcula os 1024 vizinhos mais próximos de 128 pontos de 300 dimensoes em base de dados de 400mil pontos
 #Experiencia 1:
@@ -8,5 +12,6 @@
 
 for i in {1..10}  # 10 vezes cada
 do
+    echo "-------- EXECUCAO $i --------"
     mpirun -np 8 ./main nq=128 npp=400000 d=300 k=1024
 done
